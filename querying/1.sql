e7abeeecb4eb7a0e61cb3d512aa56eccdeda3b9b
Prashant-Kumar-321 <pk6176838@gmail.com>
2024-06-08 14:10:38 +0530

    Initial Commit DISTINCT COUNT() SUBQUERY q1(q2(q3))

---
+++ querying/1.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (id, title, author, published_year) 
+VALUES (202, 'Journey to Heaven', 'Maxwell Kholi', 2002), 
+(203, 'Godan', 'Premchand', 1900); 
---
+++ querying/Library/insertData.sql
@@ -0,0 +1,17 @@
+INSERT INTO "Users" ("first_name", "last_name", "library_id", "contact", "email")
+VALUES 
+	('Shyama', 'Kumari', '2021134B', '78565109054', 'shyama76@kumariExample.com'), 
+	('Chaman', 'Mahto', '2002206A', '1935276950', 'chaman345@example.com'), 
+	('Nikhil', 'Uraon', '20241930B', '6937852799', 'nikhilOP@gmail.com')
+; 
+
+INSERT INTO "Users" ("first_name", "middle_name", "last_name", "library_id", "contact", "email")
+VALUES 
+	('Ravikant', NULL, 'Sharma', '2007375C', '2953043768', 'ravi909@gmail.com')
+; 
+
+	-- Table or tables columns' name are represented  in double quote 
+	-- String is represented by single quote 
+
+	-- /*........*/   --> double line comment 
+	
