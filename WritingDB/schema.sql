707e985ba8b0c9a34026d70e4a46a7a8b7642f39
pk123 <pk6176838@gmail.com>
2024-09-05 14:02:10 +0530

    Delete data from database/ foreign key contraints enable and disable

--- WritingDB/schema.sql
+++ WritingDB/schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE "collections"(
     acquired NUMERIC, 
     PRIMARY KEY(id)
 ); 
+
+CREATE TABLE "artists"(
+    id INTEGER, 
+    name TEXT NOT NULL, 
+    PRIMARY KEY(id) 
+); 
+
+CREATE TABLE "tem_created"(
+    artist_id INTEGER, 
+    collection_id INTEGER, 
+    PRIMARY KEY(artist_id, collection_id), 
+    FOREIGN KEY(artist_id) REFERENCES artists(id) ON DELETE CASCADE, 
+    FOREIGN KEY(collection_id) REFERENCES collections(id) ON DELETE CASCADE
+); 
